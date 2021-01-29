d2e429701c5847508e38e7ee559f40274f5468fd
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2021-01-29 12:49:05 -0500

    pad transaction deadline

--- src/hooks/useTransactionDeadline.ts
+++ src/hooks/useTransactionDeadline.ts
@@ -5,7 +5,7 @@ import { AppState } from '../state'
 // combines the current timestamp with the user setting to give the deadline that should be used for any submitted transaction
 export default function useTransactionDeadline(): BigNumber | undefined {
   const ttl = useSelector<AppState, number>(state => state.user.userDeadline)
-  const currentTimestamp = BigNumber.from(new Date().getTime())
+  const currentTimestamp = BigNumber.from(new Date().getTime() + 100000)
   if (currentTimestamp && ttl) return currentTimestamp.add(ttl)
   return undefined
 }
