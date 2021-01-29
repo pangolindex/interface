b06576e220093a0ef6b5b2fe72817bb2e50df43e
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2021-01-29 12:21:56 -0500

    use PGL

--- src/components/earn/StakingModal.tsx
+++ src/components/earn/StakingModal.tsx
@@ -242,7 +242,7 @@ export default function StakingModal({ isOpen, onDismiss, stakingInfo, userLiqui
 				<LoadingView onDismiss={wrappedOnDismiss}>
 					<AutoColumn gap="12px" justify={'center'}>
 						<TYPE.largeHeader>Depositing Liquidity</TYPE.largeHeader>
-						<TYPE.body fontSize={20}>{parsedAmount?.toSignificant(4)} PNG</TYPE.body>
+						<TYPE.body fontSize={20}>{parsedAmount?.toSignificant(4)} PGL</TYPE.body>
 					</AutoColumn>
 				</LoadingView>
 			)}
@@ -250,7 +250,7 @@ export default function StakingModal({ isOpen, onDismiss, stakingInfo, userLiqui
 				<SubmittedView onDismiss={wrappedOnDismiss} hash={hash}>
 					<AutoColumn gap="12px" justify={'center'}>
 						<TYPE.largeHeader>Transaction Submitted</TYPE.largeHeader>
-						<TYPE.body fontSize={20}>Deposited {parsedAmount?.toSignificant(4)} PNG</TYPE.body>
+						<TYPE.body fontSize={20}>Deposited {parsedAmount?.toSignificant(4)} PGL</TYPE.body>
 					</AutoColumn>
 				</SubmittedView>
 			)}
