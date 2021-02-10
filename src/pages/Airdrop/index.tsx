a0c017993752fb4d07a59fae67adcfac8f1ee97f
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2021-02-10 11:37:26 -0500

    display claim amount even if they can't withdraw

--- src/pages/Airdrop/index.tsx
+++ src/pages/Airdrop/index.tsx
@@ -118,6 +118,9 @@ export default function Vote() {
 						<TYPE.body color={theme.text1} textAlign="center">
 							You have no UNI or SUSHI tokens. Please follow the tutorial here to add UNI or SUSHI tokens to your wallet.
            				</TYPE.body>
+						<TYPE.body mt="1rem" color={theme.text1} textAlign="center">
+							{"You have" + claimAmount?.toFixed(0, { groupSeparator: ',' }) + " PNG available to claim"}
+						</TYPE.body>
 					</Card>
 				) : attempting ? (
 					<EmptyProposals>
