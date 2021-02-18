fbc1d901cbbaea6fd56fb95e932a331fed0ccb48
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2021-02-18 15:31:05 -0500

    move gas price to constants

--- src/components/GasFeeAlert.tsx
+++ src/components/GasFeeAlert.tsx
@@ -5,6 +5,7 @@ import { transparentize } from 'polished'
 import { AlertTriangle } from 'react-feather'
 import { TYPE } from '../theme'
 import { RowFixed } from './Row'
+import { GAS_PRICE } from '../constants'
 
 export const GasFee = styled(AutoColumn)`
   background-color: ${({ theme }) => transparentize(0.9, theme.primary1)};
@@ -21,7 +22,7 @@ export default function GasFeeAlert() {
 		<GasFee style={{ marginTop: '16px' }} justify="flex-start" gap={'0px'}>
 			<RowFixed>
 				<AlertTriangle size={20} style={{ marginRight: '8px', minWidth: 24 }} />
-				<TYPE.main color={theme.primary1}> Transactions will not be accepted unless the gas fee is set to 470 gwei.</TYPE.main>
+				<TYPE.main color={theme.primary1}> Transactions will not be accepted unless the gas fee is set to {GAS_PRICE} gwei.</TYPE.main>
 			</RowFixed>
 		</GasFee>
 	)
