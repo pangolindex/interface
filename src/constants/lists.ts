cfd64712cabe638c831107c48d2be2398766d1c4
Connor Daly <connor.daly@avalabs.org>
2021-03-26 10:59:28 -0400

    Added stablecoin list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -2,9 +2,11 @@
 export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/aeb.tokenlist.json'
 export const TOP_15_TOKEN_List = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/top15.tokenlist.json'
 export const DEFI_TOKEN_LIST = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/defi.tokenlist.json'
+export const STABLECOIN_TOKEN_LIST = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/stablecoin.tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   TOP_15_TOKEN_List,
   DEFAULT_TOKEN_LIST_URL,
-  DEFI_TOKEN_LIST
+  DEFI_TOKEN_LIST,
+  STABLECOIN_TOKEN_LIST
 ]
