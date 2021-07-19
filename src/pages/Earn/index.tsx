0e6b41c4700c1302f59fd5d30bd62af64c5b49f9
Brandon <brandonmino@bellsouth.net>
2021-07-19 12:39:13 -0400

    Squashed changes

--- src/pages/Earn/index.tsx
+++ src/pages/Earn/index.tsx
@@ -1,10 +1,10 @@
 import React, { useMemo, useState } from 'react'
 import { AutoColumn } from '../../components/Column'
 import styled from 'styled-components'
-import { STAKING_REWARDS_INFO, useStakingInfo } from '../../state/stake/hooks'
+import { MIGRATIONS, STAKING_REWARDS_INFO, useStakingInfo } from '../../state/stake/hooks'
 import { TYPE, ExternalLink } from '../../theme'
 import PoolCard from '../../components/earn/PoolCard'
-import { RouteComponentProps, NavLink } from 'react-router-dom'
+import { RouteComponentProps } from 'react-router-dom'
 import { RowBetween } from '../../components/Row'
 import { CardSection, DataCard, CardNoise, CardBGImage } from '../../components/earn/styled'
 import Loader from '../../components/Loader'
@@ -45,23 +45,19 @@ export default function Earn({
     Promise.all(
       stakingInfos
         ?.sort(function(info_a, info_b) {
+          // only first has ended
+          if (info_a.isPeriodFinished && !info_b.isPeriodFinished) return 1
+          // only second has ended
+          if (!info_a.isPeriodFinished && info_b.isPeriodFinished) return -1
           // greater stake in avax comes first
           return info_a.totalStakedInWavax?.greaterThan(info_b.totalStakedInWavax ?? JSBI.BigInt(0)) ? -1 : 1
         })
         .sort(function(info_a, info_b) {
-          if (info_a.stakedAmount.greaterThan(JSBI.BigInt(0))) {
-            if (info_b.stakedAmount.greaterThan(JSBI.BigInt(0)))
-              // both are being staked, so we keep the previous sorting
-              return 0
-            // the second is actually not at stake, so we should bring the first up
-            else return -1
-          } else {
-            if (info_b.stakedAmount.greaterThan(JSBI.BigInt(0)))
-              // first is not being staked, but second is, so we should bring the first down
-              return 1
-            // none are being staked, let's keep the  previous sorting
-            else return 0
-          }
+          // only the first is being staked, so we should bring the first up
+          if (info_a.stakedAmount.greaterThan(JSBI.BigInt(0)) && !info_b.stakedAmount.greaterThan(JSBI.BigInt(0))) return -1
+          // only the second is being staked, so we should bring the first down
+          if (!info_a.stakedAmount.greaterThan(JSBI.BigInt(0)) && info_b.stakedAmount.greaterThan(JSBI.BigInt(0))) return 1
+          return 0
         })
         .map(stakingInfo => {
           return fetch(`https://api.pangolin.exchange/pangolin/apr/${stakingInfo.stakingRewardAddress}`)
@@ -107,34 +103,11 @@ export default function Earn({
           <CardBGImage />
           <CardNoise />
         </DataCard>
-        <DataCard>
-          <CardNoise />
-          <CardSection>
-            <AutoColumn gap="md">
-              <RowBetween>
-                <TYPE.white fontWeight={600}>{t('earnPage.importantUpdate')}</TYPE.white>
-              </RowBetween>
-              <RowBetween>
-                <TYPE.white fontSize={14}>{t('earnPage.pangolinGovernanceProposalResult')}</TYPE.white>
-              </RowBetween>
-              <RowBetween>
-                <TYPE.white fontSize={14}>{t('earnPage.toUnstake')}</TYPE.white>
-              </RowBetween>{' '}
-              <NavLink style={{ color: 'white', textDecoration: 'underline' }} to="/png/0" target="_blank">
-                <TYPE.white fontSize={14}>{t('earnPage.oldPngPools')}</TYPE.white>
-              </NavLink>
-              <NavLink style={{ color: 'white', textDecoration: 'underline' }} to="/png/1" target="_blank">
-                <TYPE.white fontSize={14}>{t('earnPage.newPngPools')}</TYPE.white>
-              </NavLink>
-            </AutoColumn>
-          </CardSection>
-        </DataCard>
       </TopSection>
 
       <AutoColumn gap="lg" style={{ width: '100%', maxWidth: '720px' }}>
         <DataRow style={{ alignItems: 'baseline' }}>
           <TYPE.mediumHeader style={{ marginTop: '0.5rem' }}>{t('earnPage.participatingPools')}</TYPE.mediumHeader>
-          <TYPE.black fontWeight={400}>{t('earnPage.rewardsNeverEnd')}</TYPE.black>
         </DataRow>
 
         <PoolSection>
@@ -148,6 +121,7 @@ export default function Earn({
                 apr={stakingInfo.apr}
                 key={stakingInfo.stakingRewardAddress}
                 stakingInfo={stakingInfo}
+                migration={MIGRATIONS.find(migration => migration.from.stakingRewardAddress === stakingInfo.stakingRewardAddress)?.to}
                 version={version}
               />
             ))
