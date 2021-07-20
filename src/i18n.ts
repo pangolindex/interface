fa318cc66fa01b705896073ec7628665fa8efa05
Brandon <brandonmino@bellsouth.net>
2021-07-20 12:44:06 -0400

    Remove fake "jp" locale
    English masquerading as Japanese

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,7 +3,7 @@ import { initReactI18next } from 'react-i18next'
 import XHR from 'i18next-xhr-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
-export const availableLanguages = ['en', 'de', 'tr', 'zh-cn', 'jp', 'es', 'fr']
+export const availableLanguages = ['en', 'de', 'tr', 'zh-cn', 'es', 'fr']
 
 i18next
   .use(XHR)
