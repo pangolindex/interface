19328036fb7b14f522db1ddd54180420e17c9fc9
Shungy <35086804+Shungy@users.noreply.github.com>
2021-08-19 09:12:07 -0400

    add QI token address

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -176,7 +176,7 @@ export const DYP: { [chainId in ChainId]: Token } = {
 
 export const QI: { [chainId in ChainId]: Token } = {
   [ChainId.FUJI]: new Token(ChainId.FUJI, ZERO_ADDRESS, 18, 'QI', 'BENQI'),
-  [ChainId.AVALANCHE]: new Token(ChainId.AVALANCHE, ZERO_ADDRESS, 18, 'QI', 'BENQI')
+  [ChainId.AVALANCHE]: new Token(ChainId.AVALANCHE, 0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5, 18, 'QI', 'BENQI')
 }
 
 export const WALBT: { [chainId in ChainId]: Token } = {
