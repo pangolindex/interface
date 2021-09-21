00d7167c2b36bab380efd6eac31a7ce6a14de582
Alex Harley <alexbruceharley@gmail.com>
2021-09-21 19:17:11 +0200

    fix: use C chain ID not Ethereum mainnet

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,7 +1,7 @@
 import { Web3Provider } from '@ethersproject/providers'
 import { InjectedConnector } from '@pangolindex/web3-react-injected-connector'
 import { WalletLinkConnector } from '@web3-react/walletlink-connector'
-import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { NetworkConnector } from './NetworkConnector'
 
 const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
@@ -32,6 +32,10 @@ export const walletlink = new WalletLinkConnector({
   appLogoUrl: 'https://raw.githubusercontent.com/pangolindex/interface/master/public/images/384x384_App_Icon.png'
 })
 
-export const walletconnect = new WalletConnectConnector({ rpc: {
-    1: NETWORK_URL
-  }, qrcode: true, bridge: 'https://bridge.walletconnect.org'})
+export const walletconnect = new WalletConnectConnector({
+  rpc: {
+    43114: NETWORK_URL
+  },
+  qrcode: true,
+  bridge: 'https://bridge.walletconnect.org'
+})
