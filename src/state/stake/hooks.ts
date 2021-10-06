8624c2af3797d9569b32625822ae2d9ad63ad173
Brandon <brandon@pangolin.exchange>
2021-10-06 11:13:37 -0400

    Formatting

--- src/state/stake/hooks.ts
+++ src/state/stake/hooks.ts
@@ -108,23 +108,23 @@ export interface BridgeMigrator {
 const SINGLE_SIDE_STAKING: { [key: string]: SingleSideStaking } = {
   WAVAX_V0: {
     rewardToken: WAVAX[ChainId.AVALANCHE],
-	  conversionRouteHops: [],
-    stakingRewardAddress: '0xD49B406A7A29D64e081164F6C3353C599A2EeAE9',
+    conversionRouteHops: [],
+    stakingRewardAddress: "0xD49B406A7A29D64e081164F6C3353C599A2EeAE9",
     version: 0
   },
-	OOE_V0: {
-		rewardToken: OOE[ChainId.AVALANCHE],
+  OOE_V0: {
+    rewardToken: OOE[ChainId.AVALANCHE],
     conversionRouteHops: [WAVAX[ChainId.AVALANCHE]],
-		stakingRewardAddress: '0xf0eFf017644680B9878429137ccb2c041b4Fb701',
-		version: 0
-	},
+    stakingRewardAddress: "0xf0eFf017644680B9878429137ccb2c041b4Fb701",
+    version: 0
+  },
   APEIN_V0: {
     rewardToken: APEIN[ChainId.AVALANCHE],
     conversionRouteHops: [WAVAX[ChainId.AVALANCHE]],
-    stakingRewardAddress: '0xfe1d712363f2B1971818DBA935eEC13Ddea474cc',
+    stakingRewardAddress: "0xfe1d712363f2B1971818DBA935eEC13Ddea474cc",
     version: 0
-  },
-}
+  }
+};
 
 const DOUBLE_SIDE_STAKING: { [key: string]: DoubleSideStaking } = {
   WAVAX_ETH_V0: {
