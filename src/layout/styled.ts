fac284e3262be8d5ec5a9b49235b96bae8bf3ff3
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2021-11-15 23:04:00 +0530

    seperate style file for layout

---
+++ src/layout/styled.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import { Box } from '@pangolindex/components'
+
+export const MainContent = styled.div<{ collapsed: boolean }>`
+  &&& {
+    min-height: 100vh;
+    margin-left: ${({ collapsed }) => (collapsed ? '70px' : '220px')};
+    width: ${({ collapsed }) => `calc(100% - ${collapsed ? 70 : 220}px)`};
+  }
+`
+
+export const AppContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding-top: 100px; 
+  padding: 50px
+  height: 100%;
+  flex: 1;
+  overflow-y: auto;
+  overflow-x: hidden;
+  z-index: 10;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    padding:16px;
+    padding-top: 2rem; 
+  `};
+
+  z-index: 1;
+`
+
+export const Wrapper = styled(Box)`
+  &&& {
+    min-height: 100vh;
+  }
+`
