1fcd77aee900eb6a45555f8358b7837894c93bc3
flashdebugger <flashdebugger0330@gmail.com>
2021-11-18 08:13:22 -0500

    Add basic html structure for pangolin v2 dashboard

--- src/layout/Sidebar/index.tsx
+++ src/layout/Sidebar/index.tsx
@@ -18,7 +18,7 @@ import {
 } from './styled'
 import Backward from '../../assets/svg/backward.svg'
 import Forward from '../../assets/svg/forward.svg'
-import { Swap, Stake, Pool, Farm, Buy, Vote, Migration } from '../../components/Icons'
+import { Dashboard, Swap, Stake, Pool, Farm, Buy, Vote, Migration } from '../../components/Icons'
 import Charts from '../../assets/svg/menu/analytics.svg'
 import Partners from '../../assets/svg/menu/partners.svg'
 import { ANALYTICS_PAGE } from '../../constants'
@@ -38,6 +38,13 @@ export default function Sidebar({ collapsed, onCollapsed }: SidebarProps) {
   const location: any = useLocation()
   const theme = useContext(ThemeContext)
   const mainLinks = [
+    {
+      link: '/beta/dashboard',
+      icon: Dashboard,
+      title: t('header.dashboard'),
+      id: 'dashboard',
+      isActive: location?.pathname?.startsWith('/beta/dashboard')
+    },
     {
       link: '/swap',
       icon: Swap,
