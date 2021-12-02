dd51dcd0489e07a87275fe5c97fbfaa30b9b9c31
Brandon <brandon@pangolin.exchange>
2021-12-02 14:29:17 -0500

    Support multiple default tokenlists

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,15 +1,15 @@
 // the Pangolin Default token list lives here
 export const AEB_TOKENLIST = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/aeb.tokenlist.json'
-export const TOP_15_TOKEN_List = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/top15.tokenlist.json'
+export const TOP_15_TOKEN_LIST = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/top15.tokenlist.json'
 export const DEFI_TOKEN_LIST = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/defi.tokenlist.json'
 export const STABLECOIN_TOKEN_LIST = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/stablecoin.tokenlist.json'
 export const AVAX_BRIDGE_LIST = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/ab.tokenlist.json'
-export const DEFAULT_TOKEN_LIST_URL = AVAX_BRIDGE_LIST
 
-export const DEFAULT_LIST_OF_LISTS: string[] = [
-  TOP_15_TOKEN_List,
+export const DEFAULT_TOKEN_LISTS_SELECTED: string[] = [AVAX_BRIDGE_LIST, DEFI_TOKEN_LIST]
+
+export const DEFAULT_TOKEN_LISTS: string[] = [
+  TOP_15_TOKEN_LIST,
   AEB_TOKENLIST,
-  DEFAULT_TOKEN_LIST_URL,
   DEFI_TOKEN_LIST,
   STABLECOIN_TOKEN_LIST,
   AVAX_BRIDGE_LIST
