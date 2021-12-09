85d14679257639b89c2bf21f5b474c6441fb257d
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2021-12-09 17:41:01 +0530

    add ui for watchlist

--- src/pages/Beta/Swap/WatchList/index.tsx
+++ src/pages/Beta/Swap/WatchList/index.tsx
@@ -1,61 +1,44 @@
-import React, { useContext } from 'react'
-import { PageWrapper, GridContainer, Divider } from './styleds'
-import { Text, Box, DoubleCurrencyLogo } from '@pangolindex/components'
-import { ChainId, Token } from '@pangolindex/sdk'
+import React, { useContext, useState } from 'react'
+import { Text, Box, Button } from '@pangolindex/components'
+import { ChainId } from '@pangolindex/sdk'
 import { Plus } from 'react-feather'
 import { ThemeContext } from 'styled-components'
+import { LINK, PNG, TIME, AAVEe, APEIN, BIFI, PEFI } from 'src/constants'
+import { useActiveWeb3React } from 'src/hooks'
+import WatchlistRow from './WatchlistRow'
+import { WatchListRoot, GridContainer } from './styleds'
+import Scrollbars from 'react-custom-scrollbars'
+import CoinChart from './CoinChart'
 
 const WatchList = () => {
-  const currency0 = new Token(ChainId.AVALANCHE, '0xf20d962a6c8f70c731bd838a3a388D7d48fA6e15', 18, 'ETH', 'Ether')
-  const currency1 = new Token(ChainId.AVALANCHE, '0x60781C2586D68229fde47564546784ab3fACA982', 18, 'PNG', 'Pangolin')
+  const { chainId = ChainId.AVALANCHE } = useActiveWeb3React()
+  const coins = [PNG, LINK, TIME, AAVEe, APEIN, BIFI, PEFI].map(coin => coin[chainId])
   const theme = useContext(ThemeContext)
+  const [selectedToken, setSelectedToken] = useState(coins[0])
+
   return (
-    <PageWrapper>
+    <WatchListRoot>
       <Box display="flex" alignItems="center" justifyContent="space-between">
         <Text color="text1" fontSize={32} fontWeight={500}>
           WatchList
         </Text>
-        <Box bgColor={theme.bg5 as any} p={"5px"}>
-          <Plus size={12} color={theme.text1} />
+        <Box bgColor={theme.bg5 as any} p={'5px'}>
+          <Button variant="primary" backgroundColor="text8" color="text1" width={'32px'} height={'32px'} padding="0px">
+            <Plus size={12} color={theme.text1} />
+          </Button>
         </Box>
       </Box>
       <GridContainer>
-        <Box>Chart</Box>
+        <CoinChart coin={selectedToken} />
         <Box>
-          <Box display="flex" alignItems="center" justifyContent="space-between">
-            <Box display="flex" alignItems="center">
-              <DoubleCurrencyLogo size={32} currency0={currency0} currency1={currency1} />
-              <Text color="text1" fontSize={20} fontWeight={500} marginLeft={10}>
-                AVAX
-              </Text>
-            </Box>
-
-            <Box>
-              <Text color="text1" fontSize={16} fontWeight={500} marginLeft={10}>
-                $227.05M
-              </Text>
-            </Box>
-          </Box>
-
-          <Divider />
-
-          <Box display="flex" alignItems="center" justifyContent="space-between">
-            <Box display="flex" alignItems="center">
-              <DoubleCurrencyLogo size={32} currency0={currency0} currency1={currency1} />
-              <Text color="text1" fontSize={20} fontWeight={500} marginLeft={10}>
-                AVAX
-              </Text>
-            </Box>
-
-            <Box>
-              <Text color="text1" fontSize={16} fontWeight={500} marginLeft={10}>
-                $227.05M
-              </Text>
-            </Box>
-          </Box>
+          <Scrollbars>
+            {coins.map(coin => (
+              <WatchlistRow coin={coin} key={coin.address} onClick={() => setSelectedToken(coin)} />
+            ))}
+          </Scrollbars>
         </Box>
       </GridContainer>
-    </PageWrapper>
+    </WatchListRoot>
   )
 }
 export default WatchList
--- src/pages/Beta/Swap/WatchList/styleds.tsx
+++ src/pages/Beta/Swap/WatchList/styleds.tsx
@@ -1,23 +1,69 @@
 import { Box } from '@pangolindex/components'
 import styled from 'styled-components'
 
-export const PageWrapper = styled(Box)`
+export const WatchListRoot = styled(Box)`
   width: 100%;
   border-radius: 10px;
   padding: 20px;
   background-color: ${({ theme }) => theme.bg2};
+  display: flex;
+  flex-direction: column;
 `
 
 export const GridContainer = styled(Box)`
   display: grid;
-  grid-template-columns: auto auto;
+  grid-template-columns: minmax(auto, 60%) 40%;
   grid-gap: 12px;
-  padding: 10px;
-  margin-top: 20px;
+  padding: 10px 0px;
+  flex: 1;
 `
+
 export const Divider = styled(Box)`
   height: 1px;
   background-color: ${({ theme }) => theme.bg7};
   margin: 10px 0px 10px 0px;
   width: 100%;
+`
+
+export const CoinList = styled(Box)`
+  max-height: 200px;
+  overflow-y: auto;
+`
+
+// WatchList Row Styles
+
+export const RowWrapper = styled(Box)`
+  padding: 15px 10px;
+  display: grid;
+  grid-template-columns: 100px minmax(auto, calc(100% - 15px)) 50px;
+  align-items: center;
+  border-bottom: 1px solid ${({ theme }) => theme.text9};
+  cursor: pointer;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.bg6};
+  }
+`
+
+// Coin Chart Styles
+
+export const SelectedCoinInfo = styled(Box)`
+  display: grid;
+  grid-template-columns: max-content auto max-content;
+  grid-gap: 15px;
+  align-items: center;
+`
+
+export const TrackIcons = styled(Box)`
+  display: grid;
+  grid-template-columns: max-content max-content;
+  grid-gap: 15px;
+  align-items: center;
+`
+
+export const DurationBtns = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `
