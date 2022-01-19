1ae80ef32171d78441181ebcedf3dbc247e10442
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2022-01-19 14:16:24 +0530

    add dummy log

--- src/pages/Earn/Earn.tsx
+++ src/pages/Earn/Earn.tsx
@@ -106,6 +106,7 @@ const Earn: React.FC<EarnProps> = ({ version, stakingInfos, poolMap }) => {
   }, [poolCards, debouncedSearchQuery])
 
   useEffect(() => {
+    console.log('loading farms...')
     Promise.all(
       stakingInfoData.sort(function(info_a, info_b) {
         if (sortBy.field === SortingType.totalStakedInUsd) {
