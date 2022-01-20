b61007e0fe9a0643faa82f8e5b7e9038af0e9f39
Pedro <pedroantonioppms@gmail.com>
2022-01-20 10:23:49 -0400

    Fix: check if account exist

--- src/state/portifolio/hooks.ts
+++ src/state/portifolio/hooks.ts
@@ -3,60 +3,62 @@ import { useEffect, useState } from 'react'
 import { CHAINS } from 'src/constants/chains'
 import { useActiveWeb3React } from 'src/hooks'
 
-interface ResponseChain{
-    id: string
-    usd_value: Number
+interface ResponseChain {
+  id: string
+  usd_value: Number
 }
 
 // Get the USD balance of address of all chains (supported by Debank)
 export function useGetChainsBalances() {
-    const [balances, setBalances] = useState<{}>()
+  const [balances, setBalances] = useState<{}>()
 
-    const { account } = useActiveWeb3React()
+  const { account } = useActiveWeb3React()
 
-    useEffect(() => {
-      const getBalances = async () => {
-        const  response = await fetch(`https://openapi.debank.com/v1/user/total_balance?id=${account}`)
-        const data = await response.json()
-        let chainbalances: any = {
-            all: data.total_usd_value
-        }
-        console.log(chainbalances)
-        data.chain_list.forEach((chain: ResponseChain) => {
-            chainbalances[chain.id] = chain.usd_value
-        });
-        setBalances(chainbalances)
+  useEffect(() => {
+    const getBalances = async () => {
+      const response = await fetch(`https://openapi.debank.com/v1/user/total_balance?id=${account}`)
+      const data = await response.json()
+      let chainbalances: any = {
+        all: data.total_usd_value
       }
 
+      data.chain_list.forEach((chain: ResponseChain) => {
+        chainbalances[chain.id] = chain.usd_value
+      });
+      setBalances(chainbalances)
+    }
+    if (!!account) {
       getBalances()
+    }
+  }, [account, setBalances])
 
-    }, [account, setBalances])
-  
-    return balances
+  return balances
 }
 
 // Get the USD balance of address of connected chain
 export function useGetChainBalance() {
-    const [balance, setBalance] = useState<number>(0)
+  const [balance, setBalance] = useState<number>(0)
 
-    let { chainId = ChainId.AVALANCHE , account } = useActiveWeb3React()
+  let { chainId = ChainId.AVALANCHE, account } = useActiveWeb3React()
 
-    if (chainId === ChainId.FUJI){
-      chainId = ChainId.AVALANCHE
+  if (chainId === ChainId.FUJI) {
+    chainId = ChainId.AVALANCHE
+  }
+
+  const chain = CHAINS[chainId]
+  useEffect(() => {
+    const getBalance = async () => {
+      const response = await fetch(`https://openapi.debank.com/v1/user/chain_balance?id=${account}&chain_id=${chain.symbol.toLowerCase()}`)
+      const data = await response.json()
+
+      setBalance(data.usd_value)
     }
 
-    const chain = CHAINS[chainId]
-    useEffect(() => {
-      const getBalance = async () => {
-        const  response = await fetch(`https://openapi.debank.com/v1/user/chain_balance?id=${account}&chain_id=${chain.symbol.toLowerCase()}`)
-        const data = await response.json()
-        console.log(data)
-        setBalance(data.usd_value)
-      }
-
+    if (!!account) {
       getBalance()
-  
-    }, [account, chain, setBalance])
-  
-    return balance
+    }
+
+  }, [account, chain, setBalance])
+
+  return balance
 }
