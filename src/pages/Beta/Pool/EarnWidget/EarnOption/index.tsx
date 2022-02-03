18d48857b1cfda0108ae0ef9d79d273bd8db1ada
Pedro <pedroantonioppms@gmail.com>
2022-02-03 16:29:30 -0400

    Update Translations

--- src/pages/Beta/Pool/EarnWidget/EarnOption/index.tsx
+++ src/pages/Beta/Pool/EarnWidget/EarnOption/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Text, Box, ToggleButtons } from '@pangolindex/components'
 import { EarnWrapper } from './styled'
+import { useTranslation } from 'react-i18next'
 
 interface Props {
   type: string
@@ -8,15 +9,21 @@ interface Props {
 }
 
 const TradeOption: React.FC<Props> = ({ type, setType }) => {
+  const { t } = useTranslation()
   return (
     <EarnWrapper>
       <Box p={10}>
         <Box display="flex" alignItems="center" justifyContent="space-between">
-          <Text color="text1" fontSize={24} fontWeight={500}>
-            {type === 'POOLS' ? 'Pools' : 'Farms'}
+          <Text
+            color="text1"
+            fontSize={20}
+            fontWeight={500}
+            style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
+          >
+            {type === 'Pool' ? t('pool.addLiquidity') : t('header.farm')}
           </Text>
           <ToggleButtons
-            options={['POOLS', 'FARMS']}
+            options={['Pool', 'Farm']}
             value={type}
             onChange={value => {
               setType(value)
