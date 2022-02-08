4cace3b4aaaf7d4f112f79f3022eaee7856603b4
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2022-02-08 15:49:13 +0530

    fix beta url link

--- src/components/Header/URLWarning.tsx
+++ src/components/Header/URLWarning.tsx
@@ -5,6 +5,7 @@ import { AlertTriangle, X } from 'react-feather'
 import { useURLWarningToggle, useURLWarningVisible } from '../../state/user/hooks'
 import { isMobile } from 'react-device-detect'
 import { useTranslation } from 'react-i18next'
+import { useIsBetaUI } from '../../hooks/useLocation'
 
 const PhishAlert = styled.div<{ isActive: any }>`
   width: 100%;
@@ -27,17 +28,19 @@ export default function URLWarning() {
   const toggleURLWarning = useURLWarningToggle()
   const showURLWarning = useURLWarningVisible()
   const { t } = useTranslation()
-
+  const isBeta = useIsBetaUI()
   return isMobile ? (
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> {t('header.makeSureURLWarning')}
         <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>app.pangolin.exchange</code>
-        <Text as="a" href="/beta/swap">
-          <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>
-            {t('header.tryOurNewBetaSite')}
-          </code>
-        </Text>
+        {!isBeta && (
+          <Text as="a" href="#/beta/swap">
+            <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>
+              {t('header.tryOurNewBetaSite')}
+            </code>
+          </Text>
+        )}
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
     </PhishAlert>
