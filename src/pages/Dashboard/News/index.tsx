2393fc58702c897216d360a290954fb09a77233e
jb1011 <demenet.jb@hotmail.fr>
2022-03-07 14:17:15 +0100

    change box height of news

--- src/pages/Dashboard/News/index.tsx
+++ src/pages/Dashboard/News/index.tsx
@@ -39,7 +39,7 @@ export default function NewsWidget() {
 
   return (
     <NewsSection img={Earth}>
-      <Box height="15%" display="flex">
+      <Box height="25%" display="flex">
         <NewsTitle>News</NewsTitle>
         <SlickNext onClick={handleNewsBack} style={{ right: 60 }}>
           <ArrowLeft size={20} style={{ minWidth: 24 }} />
