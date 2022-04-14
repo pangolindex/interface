61ebdc8fa3402b681aa124c5d69ab1df7b5ec17c
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2022-04-14 11:45:16 +0530

    governance from subgraph

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,10 +1,19 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
+import { SUBGRAPH_BASE_URL } from 'src/constants'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/pangolindex/exchange'
+    uri: `${SUBGRAPH_BASE_URL}/exchange`
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true
+})
+
+export const governanceClient = new ApolloClient({
+  link: new HttpLink({
+    uri: `${SUBGRAPH_BASE_URL}/governance`
   }),
   cache: new InMemoryCache(),
   shouldBatch: true
