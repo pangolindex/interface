fdc65652d60d81393767aad41e69419b8a1c8def
jb1011 <demenet.jb@hotmail.fr>
2022-04-22 13:15:25 +0200

    remove airdrop folder to solve sonar cloud issue - update on airdrop will come in upcoming PR

--- src/pages/Beta/Airdrop2/BoxesType/index.tsx
+++
@@ -1,7 +0,0 @@
-import { BoxBuyCurrency } from './BoxBuyCurrency'
-import { BoxChangeChain } from './BoxChangeChain'
-import { BoxCheckEligibility } from './BoxCheckEligibility'
-import { BoxClaimReward } from './BoxClaimReward'
-import { BoxNotConnected } from './BoxNotConnected'
-
-export { BoxBuyCurrency, BoxChangeChain, BoxCheckEligibility, BoxClaimReward, BoxNotConnected }
