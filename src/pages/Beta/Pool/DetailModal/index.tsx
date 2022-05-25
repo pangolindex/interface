ba767081bd8c97e86faad0eac490892d4c299372
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2022-05-25 14:32:02 +0530

    reset input when detail modal change

--- src/pages/Beta/Pool/DetailModal/index.tsx
+++ src/pages/Beta/Pool/DetailModal/index.tsx
@@ -1,10 +1,13 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { ApplicationModal } from 'src/state/application/actions'
 import { useModalOpen, usePoolDetailnModalToggle } from 'src/state/application/hooks'
 import Modal from 'src/components/Beta/Modal'
 import { StakingInfo } from 'src/state/stake/hooks'
 import { ThemeContext } from 'styled-components'
 import DetailView from './DetailView'
+import { AppDispatch } from 'src/state'
+import { resetMintState } from 'src/state/mint/actions'
+import { useDispatch } from 'react-redux'
 
 export interface DetailModalProps {
   stakingInfo: StakingInfo
@@ -15,6 +18,12 @@ const DetailModal = ({ stakingInfo, version }: DetailModalProps) => {
   const detailModalOpen = useModalOpen(ApplicationModal.POOL_DETAIL)
   const togglePoolDetailModal = usePoolDetailnModalToggle()
   const theme = useContext(ThemeContext)
+
+  const dispatch = useDispatch<AppDispatch>()
+
+  useEffect(() => {
+    dispatch(resetMintState())
+  }, [detailModalOpen])
 
   return (
     <Modal isOpen={detailModalOpen} onDismiss={togglePoolDetailModal} overlayBG={theme.modalBG2}>
