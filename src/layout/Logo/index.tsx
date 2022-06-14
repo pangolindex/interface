4de50bf956be6d7e0b9d130db7f46d31ff39491e
Pedro <pedroantonioppms@gmail.com>
2022-06-14 08:35:04 -0400

    Fix logo and updated il8n

--- src/layout/Logo/index.tsx
+++ src/layout/Logo/index.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import { Box } from '@pangolindex/components'
 import Logo from 'src/assets/svg/logoIcon.svg'
-import LogoSlogan from 'src/assets/svg/logoWithSlogan.svg'
+import LogoDark from 'src/assets/svg/logoSloganDark.svg'
+import LogoLight from 'src/assets/svg/logoSloganLight.svg'
+
 import { Title, LogoWrapper } from './styled'
+import { useIsDarkMode } from 'src/state/user/hooks'
 
 interface LogoProps {
   collapsed: boolean
 }
 
 export default function LogoIcon({ collapsed }: LogoProps) {
+  const isDark = useIsDarkMode()
   return (
     <LogoWrapper collapsed={collapsed}>
       <Box>
         <Title href=".">
           {!collapsed ? (
-            <img height="28px" src={LogoSlogan} alt="logo" />
+            <img height="28px" src={isDark ? LogoDark : LogoLight} alt="logo" />
           ) : (
             <img width="28px" src={Logo} alt="logo" />
           )}
