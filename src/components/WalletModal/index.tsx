c0fb97a9d82ac4e8605bd5fc8aae5c47ead823dc
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2022-06-22 19:39:57 +0530

    fix icon in wallet modal in old ui

--- src/components/WalletModal/index.tsx
+++ src/components/WalletModal/index.tsx
@@ -291,7 +291,7 @@ export default function WalletModal({
                 link={option.href}
                 header={option.name}
                 subheader={null}
-                icon={require('../../assets/images/' + option.iconName)}
+                icon={option.iconName}
               />
             )
           }
@@ -395,7 +395,7 @@ export default function WalletModal({
               link={option.href}
               header={option.name}
               subheader={null} //use option.descriptio to bring back multi-line
-              icon={require('../../assets/images/' + option.iconName)}
+              icon={option.iconName}
             />
           )
         )
