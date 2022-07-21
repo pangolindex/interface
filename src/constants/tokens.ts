8e063ef0ee3d56184c7ebde00a6634c67c23c938
Brandon <brandon@pangolin.exchange>
2022-07-21 13:56:38 -0400

    USDC-BRZ SF

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -2014,3 +2014,12 @@ export const USDt: { [chainId in ChainId]: Token } = {
   [ChainId.NEAR_MAINNET]: new Token(ChainId.NEAR_MAINNET, ZERO_ADDRESS, 18, '', ''),
   [ChainId.NEAR_TESTNET]: new Token(ChainId.NEAR_TESTNET, ZERO_ADDRESS, 18, '', '')
 }
+
+export const BRZ: { [chainId in ChainId]: Token } = {
+  [ChainId.FUJI]: new Token(ChainId.FUJI, ZERO_ADDRESS, 4, 'BRZ', 'BRZToken'),
+  [ChainId.AVALANCHE]: new Token(ChainId.AVALANCHE, '0x491a4eB4f1FC3BfF8E1d2FC856a6A46663aD556f', 4, 'BRZ', 'BRZToken'),
+  [ChainId.WAGMI]: new Token(ChainId.WAGMI, ZERO_ADDRESS, 4, 'BRZ', 'BRZToken'),
+  [ChainId.COSTON]: new Token(ChainId.COSTON, ZERO_ADDRESS, 4, 'BRZ', 'BRZToken'),
+  [ChainId.NEAR_MAINNET]: new Token(ChainId.NEAR_MAINNET, ZERO_ADDRESS, 4, 'BRZ', 'BRZToken'),
+  [ChainId.NEAR_TESTNET]: new Token(ChainId.NEAR_TESTNET, ZERO_ADDRESS, 4, 'BRZ', 'BRZToken')
+}
