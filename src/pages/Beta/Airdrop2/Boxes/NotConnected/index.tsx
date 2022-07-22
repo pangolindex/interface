ab913f1e8ab0c0cb2cebb4d4253ab9ea6f390c32
Pedro <pedroantonioppms@gmail.com>
2022-07-22 18:47:39 -0400

    Refactor Airdrop

--- src/pages/Beta/Airdrop2/Boxes/NotConnected/index.tsx
+++ src/pages/Beta/Airdrop2/Boxes/NotConnected/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import { ClaimBox, StyledLogo, Separator, TitleWrapper, SmallSeparator, TextBottomWrapper } from '../../styleds'
-import { Text, Button } from '@pangolindex/components'
-import WgmLogo from 'src/assets/images/wgmlogo.png'
+import { Text } from '@pangolindex/components'
+import { ClaimBox, Separator, TitleWrapper, TextBottomWrapper, SmallSeparator, StyledLogo } from '../../styleds'
+import { ButtonToConnect } from '../../ButtonToConnect'
+import { AVALANCHE_MAINNET, Chain } from '@pangolindex/sdk'
 
-type IStatus = {
-  checkStatus: () => void
+interface Props {
+  chain: Chain
 }
 
-export const BoxCheckEligibility: React.FC<IStatus> = ({ checkStatus }) => {
+export const BoxNotConnected: React.FC<Props> = ({ chain }) => {
   return (
     <ClaimBox>
       <TitleWrapper>
         <Text fontSize={[28, 22]} fontWeight={700} lineHeight="33px" color="text10">
-          Claim wagmiPNG
+          Connect Your Wallet
         </Text>
-        <StyledLogo src={WgmLogo} size={'50px'} />
+        <StyledLogo src={chain?.logo ?? AVALANCHE_MAINNET?.logo} size={'50px'} />
       </TitleWrapper>
       <Separator />
       <SmallSeparator />
@@ -22,9 +23,7 @@ export const BoxCheckEligibility: React.FC<IStatus> = ({ checkStatus }) => {
         Let&apos;s check if you are eligible!
       </Text>
       <SmallSeparator />
-      <Button variant="primary" color="black" height="46px" onClick={checkStatus}>
-        CHECK IF ELIGIBLE
-      </Button>
+      <ButtonToConnect />
       <TextBottomWrapper>
         <Text fontSize={14} fontWeight={500} lineHeight="18px" color="text8">
           To be eligible or not to be eligible...
