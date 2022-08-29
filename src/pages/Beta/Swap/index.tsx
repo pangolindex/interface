f9db1924d28e8ec58fb5b7dc4392b2e994caa8e5
Bilal Corbacioglu <bilalcorbacioglu@gmail.com>
2022-08-29 16:57:33 +0100

    Added SwapType into SwapUI

--- src/pages/Beta/Swap/index.tsx
+++ src/pages/Beta/Swap/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PageWrapper, GridContainer, TopContainer, StatsWrapper } from './styleds'
 import { MyPortfolio, SwapWidget, WatchList } from '@pangolindex/components'
 import PairInfo from './PairInfo'
@@ -8,23 +8,24 @@ import { CHAINS } from '@pangolindex/sdk'
 import { isEvmChain } from 'src/utils'
 import ComingSoon from 'src/components/Beta/ComingSoon'
 import { useGelatoLimitOrdersHook } from 'src/state/swap/multiChainsHooks'
+import { SwapTypes } from '@pangolindex/components'
 
 const SwapUI = () => {
   const chainId = useChainId()
   const useGelatoLimitOrders = useGelatoLimitOrdersHook[chainId]
   const { allOrders } = useGelatoLimitOrders()
-
+  const [swapType, onSwapTypeChange] = useState(SwapTypes.MARKET)
   const isLimitOrders = (allOrders || []).length > 0
   return (
     <PageWrapper>
       <TopContainer>
         <StatsWrapper>{isEvmChain(chainId) ? <PairInfo /> : <ComingSoon />}</StatsWrapper>
-        <SwapWidget isLimitOrderVisible={CHAINS[chainId]?.mainnet} />
+        <SwapWidget onSwapTypeChange={onSwapTypeChange} isLimitOrderVisible={CHAINS[chainId]?.mainnet} />
       </TopContainer>
 
       {CHAINS[chainId]?.mainnet && isEvmChain(chainId) && (
         <GridContainer isLimitOrders={isLimitOrders}>
-          {isLimitOrders && <LimitOrderList />}
+          {isLimitOrders && swapType == SwapTypes.LIMIT && <LimitOrderList />}
           <MyPortfolio />
           <WatchList coinChartVisible={!isLimitOrders} />
         </GridContainer>
