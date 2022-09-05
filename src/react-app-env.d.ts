098665459ace7ce78dcbdcf55964b72cf0f1106d
Pedro <pedroantonioppms@gmail.com>
2022-09-05 13:14:54 -0400

    Added bitkeep

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -16,6 +16,7 @@ interface Window {
     isXDEFI?: true
     isRabby?: true
     isTalisman?: true
+    isBitKeep?: true
     on?: (...args: any[]) => void
     removeListener?: (...args: any[]) => void
     request: (...args: any[]) => Promise<any>
