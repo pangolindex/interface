82d896fdcbe7f0cd455dd04d16b31eb1303c9d92
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2022-09-06 18:12:50 +0530

    set Hedra Chain

--- src/constants/airdrop.ts
+++ src/constants/airdrop.ts
@@ -1,6 +1,7 @@
 import PNG from 'src/assets/svg/PNG/PNG.svg'
 import PNR from 'src/assets/svg/PNG/PNR.svg'
 import PSB from 'src/assets/svg/PNG/PSB.svg'
+import PNG_HEDRA from 'src/assets/svg/PNG/PNG_HEDERA.svg'
 
 import { NEAR_MAINNET, Chain, COSTON_TESTNET, SONGBIRD_CANARY, FLARE_MAINNET, ChainId } from '@pangolindex/sdk'
 
@@ -11,6 +12,7 @@ export const commingSoonAirdrops = [NEAR_MAINNET, SONGBIRD_CANARY, FLARE_MAINNET
 export const logoMapping = {
   [ChainId.COSTON]: PNG,
   [ChainId.SONGBIRD]: PSB,
+  [ChainId.HEDERA_TESTNET]: PNG_HEDRA,
   [ChainId.FUJI]: PNG,
   [ChainId.AVALANCHE]: PNG,
   [ChainId.WAGMI]: PNG,
