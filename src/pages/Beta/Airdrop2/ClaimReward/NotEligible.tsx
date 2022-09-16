90cd324ae9391e45e54342b56d4222d4e398f543
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2022-09-16 17:01:38 +0530

    update not eligible block

--- src/pages/Beta/Airdrop2/ClaimReward/NotEligible.tsx
+++ src/pages/Beta/Airdrop2/ClaimReward/NotEligible.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { AVALANCHE_MAINNET, Chain } from '@pangolindex/sdk'
-import { Box, Button, Text } from '@pangolindex/components'
+import { Chain } from '@pangolindex/sdk'
+import { Box, Text } from '@pangolindex/components'
 import { Wrapper, TextBottomWrapper } from '../styleds'
-import { switchNetwork } from 'src/utils'
 import Title from '../Title'
 
 interface Props {
@@ -12,18 +11,16 @@ interface Props {
 const NotEligible: React.FC<Props> = ({ chain }) => {
   return (
     <Wrapper>
-      <Title chain={chain} title="Try Next One!" />
+      <Title chain={chain} title="Try Next One!" subtitle="Old PSB Reimbursement 1" />
       <Box display="flex" alignItems="center" justifyContent="center" minHeight="150px">
         <Text fontSize={16} fontWeight={500} color="text1" textAlign="center">
           Sadly you are not eligible for this airdrop.
         </Text>
       </Box>
-      <Button variant="primary" color="black" height="46px" onClick={() => switchNetwork(AVALANCHE_MAINNET)}>
-        GO BACK TO AVALANCHE
-      </Button>
+
       <TextBottomWrapper>
         <Text fontSize={14} fontWeight={500} lineHeight="18px" color="text8">
-          For PNG Holder, Staker and Farmers only...
+          For {chain?.png_symbol ?? 'PNG'} Holder, Staker and Farmers only...
         </Text>
       </TextBottomWrapper>
     </Wrapper>
