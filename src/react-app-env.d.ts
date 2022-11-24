26ef83bb181aec1c18f5dceb91e032f1ebe0847b
Sarju <sarju.hansaliya@gmail.com>
2022-11-24 11:52:29 +0530

    feat: integrate core avalanche wallet (#650)
    * WIP integarte avalanche core wallet
    
    * core wallet issue resolve
    
    * component version update
    
    * fix: dummy commit
    
    * component version change
    
    * change path for svg

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -28,6 +28,15 @@ interface Window {
   }
   web3?: {}
   pendo?: any
+  avalanche?: {
+    isAvalanche?: boolean
+    once(eventName: string | symbol, listener: (...args: any[]) => void): this
+    on(eventName: string | symbol, listener: (...args: any[]) => void): this
+    off(eventName: string | symbol, listener: (...args: any[]) => void): this
+    addListener(eventName: string | symbol, listener: (...args: any[]) => void): this
+    removeListener(eventName: string | symbol, listener: (...args: any[]) => void): this
+    removeAllListeners(event?: string | symbol): this
+  }
 }
 
 declare module 'content-hash' {
