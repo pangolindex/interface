34f439653840fb4bfc9203ecf21ba54d6f564a3a
Carl Fluke <carl.fluke@coinbase.com>
2022-12-01 12:35:25 -0500

    simplify iframe and add translations

--- src/pages/Beta/Buy/index.tsx
+++ src/pages/Beta/Buy/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import { useTranslation } from '@pangolindex/components'
 import Card from 'src/components/Card'
 import { MOONPAY_PK, COINBASE_PK } from 'src/constants'
 import { useActiveWeb3React } from 'src/hooks'
@@ -9,6 +10,7 @@ import { CardTitle, Title } from '../Governance/GovernanceCard/styleds'
 import { WalletText, PayButton, CBIcon, BackButton, BackRow } from './styleds'
 
 export default function BuyV2() {
+  const { t } = useTranslation()
   const { account } = useActiveWeb3React()
   const [activeOnRamp, setActiveOnRamp] = useState('')
   const Iframe = styled.iframe`
@@ -27,13 +29,16 @@ export default function BuyV2() {
     setActiveOnRamp('')
   }
 
-  if (activeOnRamp === 'moonpay') {
-    const url = `https://buy.moonpay.io?apiKey=${MOONPAY_PK}`
+  let url
+  if (activeOnRamp === 'coinbase')
+    url = `https://pay.coinbase.com/buy/select-asset?appId=${COINBASE_PK}&destinationWallets=%5B%7B%22address%22%3A%22${walletAddr}%22%2C%22blockchains%22%3A%5B%22avalanche-c-chain%22%5D%7D%5D`
+  if (activeOnRamp === 'moonpay') url = `https://buy.moonpay.io?apiKey=${MOONPAY_PK}`
+  if (url) {
     return (
       <>
         <BackRow>
           <BackButton variant="plain" onClick={back}>
-            Back
+            {t('buyPage.back', { defaultValue: 'Back' })}
           </BackButton>
         </BackRow>
 
@@ -44,30 +49,9 @@ export default function BuyV2() {
           height="100%"
           src={url}
         >
-          <p>Your browser does not support iframes.</p>
+          <p>{t('buyPage.iframeSupport', { defaultValue: 'Your browser does not support iframes.' })}</p>
         </Iframe>
       </>
-    )
-  } else if (activeOnRamp === 'coinbase') {
-    const walletAddr = account
-    const url = `https://pay.coinbase.com/buy/select-asset?appId=${COINBASE_PK}&destinationWallets=%5B%7B%22address%22%3A%22${walletAddr}%22%2C%22blockchains%22%3A%5B%22avalanche-c-chain%22%5D%7D%5D`
-    return (
-      <div>
-        <BackRow>
-          <BackButton variant="plain" onClick={back}>
-            Back
-          </BackButton>
-        </BackRow>
-        <Iframe
-          title="Buy"
-          allow="accelerometer; autoplay; camera; gyroscope; payment"
-          width="100%"
-          height="100%"
-          src={url}
-        >
-          <p>Your browser does not support iframes.</p>
-        </Iframe>
-      </div>
     )
   } else {
     return (
@@ -78,16 +62,18 @@ export default function BuyV2() {
           </CardTitle>
           <PayButton variant="primary" onClick={() => buy('coinbase')} isDisabled={!account}>
             <CBIcon src="./images/coinbase_coin_pay_blue.svg" />
-            Buy Now
+            {t('buyPage.buy')}
           </PayButton>
-          {!account && <WalletText>Please connect your wallet</WalletText>}
+          {!account && (
+            <WalletText>{t('buyPage.connectWallet', { defaultValue: 'Please connect a wallet' })}</WalletText>
+          )}
         </Card>
         <Card>
           <CardTitle>
             <Title>MoonPay</Title>
           </CardTitle>
           <PayButton variant="primary" onClick={() => buy('moonpay')}>
-            Buy Now
+            {t('buyPage.buy', { defaultValue: 'Buy Now' })}
           </PayButton>
         </Card>
       </TopContainer>
