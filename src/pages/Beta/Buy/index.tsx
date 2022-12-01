dc459c9557ca61f52e610fe070cf7ffa0f6a4e6a
Carl Fluke <carl.fluke@coinbase.com>
2022-12-01 12:22:33 -0500

    add pay as an option on buy page

--- src/pages/Beta/Buy/index.tsx
+++ src/pages/Beta/Buy/index.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import { MOONPAY_PK } from 'src/constants'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import Card from 'src/components/Card'
+import { MOONPAY_PK, COINBASE_PK } from 'src/constants'
+import { useActiveWeb3React } from 'src/hooks'
+import { TopContainer } from 'src/pages/Dashboard/styleds'
+
+import { CardTitle, Title } from '../Governance/GovernanceCard/styleds'
+import { WalletText, PayButton, CBIcon, BackButton, BackRow } from './styleds'
 
 export default function BuyV2() {
+  const { account } = useActiveWeb3React()
+  const [activeOnRamp, setActiveOnRamp] = useState('')
   const Iframe = styled.iframe`
     border: 0;
     position: absolute;
@@ -11,16 +19,78 @@ export default function BuyV2() {
     bottom: 0;
   `
 
-  const url = `https://buy.moonpay.io?apiKey=${MOONPAY_PK}`
-  return (
-    <Iframe
-      title="Buy"
-      allow="accelerometer; autoplay; camera; gyroscope; payment"
-      width="100%"
-      height="100%"
-      src={url}
-    >
-      <p>Your browser does not support iframes.</p>
-    </Iframe>
-  )
+  const buy = (provider: string) => {
+    setActiveOnRamp(provider)
+  }
+
+  const back = () => {
+    setActiveOnRamp('')
+  }
+
+  if (activeOnRamp === 'moonpay') {
+    const url = `https://buy.moonpay.io?apiKey=${MOONPAY_PK}`
+    return (
+      <>
+        <BackRow>
+          <BackButton variant="plain" onClick={back}>
+            Back
+          </BackButton>
+        </BackRow>
+
+        <Iframe
+          title="Buy"
+          allow="accelerometer; autoplay; camera; gyroscope; payment"
+          width="100%"
+          height="100%"
+          src={url}
+        >
+          <p>Your browser does not support iframes.</p>
+        </Iframe>
+      </>
+    )
+  } else if (activeOnRamp === 'coinbase') {
+    const walletAddr = account
+    const url = `https://pay.coinbase.com/buy/select-asset?appId=${COINBASE_PK}&destinationWallets=%5B%7B%22address%22%3A%22${walletAddr}%22%2C%22blockchains%22%3A%5B%22avalanche-c-chain%22%5D%7D%5D`
+    return (
+      <div>
+        <BackRow>
+          <BackButton variant="plain" onClick={back}>
+            Back
+          </BackButton>
+        </BackRow>
+        <Iframe
+          title="Buy"
+          allow="accelerometer; autoplay; camera; gyroscope; payment"
+          width="100%"
+          height="100%"
+          src={url}
+        >
+          <p>Your browser does not support iframes.</p>
+        </Iframe>
+      </div>
+    )
+  } else {
+    return (
+      <TopContainer>
+        <Card>
+          <CardTitle>
+            <Title>Coinbase Pay</Title>
+          </CardTitle>
+          <PayButton variant="primary" onClick={() => buy('coinbase')} isDisabled={!account}>
+            <CBIcon src="./images/coinbase_coin_pay_blue.svg" />
+            Buy Now
+          </PayButton>
+          {!account && <WalletText>Please connect your wallet</WalletText>}
+        </Card>
+        <Card>
+          <CardTitle>
+            <Title>MoonPay</Title>
+          </CardTitle>
+          <PayButton variant="primary" onClick={() => buy('moonpay')}>
+            Buy Now
+          </PayButton>
+        </Card>
+      </TopContainer>
+    )
+  }
 }
---
+++ src/pages/Beta/Buy/styleds.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+import { Box, Button } from '@pangolindex/components'
+
+export const WalletText = styled(Box)`
+  color: ${({ theme }) => theme.text1};
+  margin-top: 4px;
+`
+
+export const PayButton = styled(Button)`
+  max-width: 250px;
+`
+
+export const BackButton = styled(Button)`
+  color: ${({ theme }) => theme.text1};
+  max-width: 50px;
+`
+
+export const BackRow = styled.div`
+  width: 100%;
+`
+
+export const CBIcon = styled.img`
+  width: 50px;
+  margin-right: 10px;
+`
+
+export const Container = styled(Box)`
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-gap: 12px;
+  margin-bottom: 22px;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    grid-template-columns: none;
+    grid-template-rows: max-content auto;
+    margin-bottom: 0px;
+  `};
+`
