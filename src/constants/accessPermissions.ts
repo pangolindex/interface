4bf1f1adcdb5f9746fa14342537d4a3b2d5379f2
Bilal Corbacioglu <bilalcorbacioglu@gmail.com>
2022-12-02 17:20:31 +0000

    feat: changed menu access dynamics

--- src/constants/accessPermissions.ts
+++ src/constants/accessPermissions.ts
@@ -1,30 +1,5 @@
 import { ChainId } from '@pangolindex/sdk'
-
-export const VOTE_PAGE_ACCESS: { [chainId in ChainId]: boolean } = {
-  [ChainId.FUJI]: true,
-  [ChainId.AVALANCHE]: true,
-  [ChainId.WAGMI]: true,
-  [ChainId.COSTON]: true,
-  [ChainId.SONGBIRD]: false,
-  [ChainId.HEDERA_TESTNET]: false,
-  [ChainId.NEAR_MAINNET]: false,
-  [ChainId.NEAR_TESTNET]: false,
-  [ChainId.ETHEREUM]: false,
-  [ChainId.POLYGON]: false,
-  [ChainId.FANTOM]: false,
-  [ChainId.XDAI]: false,
-  [ChainId.BSC]: false,
-  [ChainId.ARBITRUM]: false,
-  [ChainId.CELO]: false,
-  [ChainId.OKXCHAIN]: false,
-  [ChainId.VELAS]: false,
-  [ChainId.AURORA]: false,
-  [ChainId.CRONOS]: false,
-  [ChainId.FUSE]: false,
-  [ChainId.MOONRIVER]: false,
-  [ChainId.MOONBEAM]: false,
-  [ChainId.OP]: false
-}
+import { MENU_LINK } from '.'
 
 export const LIMITORDERLIST_ACCESS: { [chainId in ChainId]: boolean } = {
   [ChainId.FUJI]: true,
@@ -130,28 +105,49 @@ export const PAIRINFO_ACCESS: { [chainId in ChainId]: boolean } = {
   [ChainId.OP]: false
 }
 
-export const ONLY_BRIDGE_ACCESS: { [chainId in ChainId]: boolean } = {
-  [ChainId.FUJI]: false,
-  [ChainId.AVALANCHE]: false,
-  [ChainId.WAGMI]: false,
-  [ChainId.COSTON]: false,
-  [ChainId.SONGBIRD]: false,
-  [ChainId.HEDERA_TESTNET]: false,
-  [ChainId.NEAR_MAINNET]: false,
-  [ChainId.NEAR_TESTNET]: false,
-  [ChainId.ETHEREUM]: true,
-  [ChainId.POLYGON]: true,
-  [ChainId.FANTOM]: true,
-  [ChainId.XDAI]: true,
-  [ChainId.BSC]: true,
-  [ChainId.ARBITRUM]: true,
-  [ChainId.CELO]: true,
-  [ChainId.OKXCHAIN]: true,
-  [ChainId.VELAS]: true,
-  [ChainId.AURORA]: true,
-  [ChainId.CRONOS]: true,
-  [ChainId.FUSE]: true,
-  [ChainId.MOONRIVER]: true,
-  [ChainId.MOONBEAM]: true,
-  [ChainId.OP]: true
+const onlyBridgeHideOtherMenus: MENU_LINK[] = [
+  MENU_LINK.swap,
+  MENU_LINK.buy,
+  MENU_LINK.pool,
+  MENU_LINK.stake,
+  MENU_LINK.vote,
+  MENU_LINK.migrate,
+  MENU_LINK.airdrop,
+  MENU_LINK.stakev2
+]
+
+const nonEvmHideList: MENU_LINK[] = [
+  MENU_LINK.buy,
+  MENU_LINK.pool,
+  MENU_LINK.stake,
+  MENU_LINK.vote,
+  MENU_LINK.migrate,
+  MENU_LINK.airdrop,
+  MENU_LINK.stakev2
+]
+
+export const HIDE_MENU_ACCESS_MANAGEMENT: { [chainId in ChainId]?: MENU_LINK[] } = {
+  [ChainId.FUJI]: [],
+  [ChainId.AVALANCHE]: [],
+  [ChainId.WAGMI]: [],
+  [ChainId.COSTON]: [],
+  [ChainId.SONGBIRD]: [MENU_LINK.vote],
+  [ChainId.HEDERA_TESTNET]: nonEvmHideList,
+  [ChainId.NEAR_MAINNET]: nonEvmHideList,
+  [ChainId.NEAR_TESTNET]: nonEvmHideList,
+  [ChainId.ETHEREUM]: onlyBridgeHideOtherMenus,
+  [ChainId.POLYGON]: onlyBridgeHideOtherMenus,
+  [ChainId.FANTOM]: onlyBridgeHideOtherMenus,
+  [ChainId.XDAI]: onlyBridgeHideOtherMenus,
+  [ChainId.BSC]: onlyBridgeHideOtherMenus,
+  [ChainId.ARBITRUM]: onlyBridgeHideOtherMenus,
+  [ChainId.CELO]: onlyBridgeHideOtherMenus,
+  [ChainId.OKXCHAIN]: onlyBridgeHideOtherMenus,
+  [ChainId.VELAS]: onlyBridgeHideOtherMenus,
+  [ChainId.AURORA]: onlyBridgeHideOtherMenus,
+  [ChainId.CRONOS]: onlyBridgeHideOtherMenus,
+  [ChainId.FUSE]: onlyBridgeHideOtherMenus,
+  [ChainId.MOONRIVER]: onlyBridgeHideOtherMenus,
+  [ChainId.MOONBEAM]: onlyBridgeHideOtherMenus,
+  [ChainId.OP]: onlyBridgeHideOtherMenus
 }
--- src/layout/Sidebar/MenuLinks.tsx
+++ src/layout/Sidebar/MenuLinks.tsx
@@ -10,8 +10,7 @@ import Bridge from '../../assets/svg/menu/bridge.svg'
 import Governance from '../../assets/svg/menu/governance.svg'
 import { useLocation } from 'react-router-dom'
 import { useChainId } from 'src/hooks'
-import { CHAINS } from '@pangolindex/sdk'
-import { ONLY_BRIDGE_ACCESS, VOTE_PAGE_ACCESS } from 'src/constants/accessPermissions'
+import { hideCheck } from 'src/utils'
 
 interface Props {
   collapsed?: boolean
@@ -19,17 +18,17 @@ interface Props {
 }
 
 interface Link {
-  link: string
-  icon: string
+  link: MENU_LINK | string
+  icon: string | React.FC<Props>
   title: string
   id: string
+  isActive?: boolean
 }
 
 export const MenuLinks: React.FC<Props> = ({ collapsed = false, onClick }) => {
   const theme = useContext(ThemeContext)
   const { t } = useTranslation()
   const chainId = useChainId()
-  const chain = CHAINS[chainId]
 
   const location: any = useLocation()
 
@@ -97,21 +96,12 @@ export const MenuLinks: React.FC<Props> = ({ collapsed = false, onClick }) => {
       id: 'bridge',
       isActive: location?.pathname?.startsWith(MENU_LINK.bridge)
     }
-  ]
+  ].filter(link => !hideCheck(chainId, link.link as MENU_LINK))
 
   // for now, for non evm chain, hide all other menus except dashboard and swap
-  if (!chain.evm) {
-    mainLinks.splice(4)
-  }
-
-  if (!VOTE_PAGE_ACCESS[chainId]) {
-    const votePageIndex = mainLinks.findIndex(element => element?.id === 'vote')
-    mainLinks.splice(votePageIndex, 1)
-  }
-
-  if (ONLY_BRIDGE_ACCESS[chainId]) {
-    mainLinks.reverse().splice(2)
-  }
+  // if (!chain.evm) {
+  //   mainLinks.splice(4)
+  // }
 
   // remove stakvev2 if not exist sar contract
   if (!existSarContract(chainId)) {
@@ -156,7 +146,7 @@ export const MenuLinks: React.FC<Props> = ({ collapsed = false, onClick }) => {
     return (
       <MenuItem key={index}>
         <MenuExternalLink id={link.id} href={link.link}>
-          <img src={link.icon} width={16} alt={link.title} />
+          <img src={link.icon as string} width={16} alt={link.title} />
           {!collapsed && <MenuName fontSize={[16, 14]}>{link.title}</MenuName>}
         </MenuExternalLink>
       </MenuItem>
