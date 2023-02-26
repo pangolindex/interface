4d2e6217d55e5f58e6ea31620bdddccfefaf2267
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2023-02-27 02:03:27 +0530

    fix: migrate to vite

---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import svgr from 'vite-plugin-svgr'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+
+// https://vitejs.dev/config/
+export default () => {
+  return defineConfig({
+    plugins: [svgr(), react()],
+    define: {
+      'process.env': {}
+    },
+    resolve: {
+      alias: {
+        // here we are mapping "src" because in components we have absolute path that starts with "src"
+        src: path.resolve(__dirname, './src')
+      }
+    },
+    build: {
+      commonjsOptions: { transformMixedEsModules: true, include: [] }
+    },
+    optimizeDeps: {
+      disabled: false,
+      esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+          global: 'globalThis'
+        },
+        // Enable esbuild polyfill plugins
+        plugins: [
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          NodeGlobalsPolyfillPlugin({
+            buffer: true,
+            process: true
+          })
+        ]
+      }
+    }
+  })
+}
