cb5f4c03dd1ce6711c476473e948b7165bb5194e
Bilal Corbacioglu <bilalcorbacioglu@gmail.com>
2023-04-30 21:28:08 +0300

    feat: test embed

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,37 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from './Header'
-import Sidebar from './Sidebar'
-import { Wrapper, MainContent, AppContent } from './styled'
-import Footer from './Footer'
-import URLWarning from 'src/components/Header/URLWarning'
-import MobileMenu from './Header/MobileMenu'
+import { Wrapper } from './styled'
 
 const Layout: React.FC<unknown> = () => {
-  const [isDrawerCollapsed, setIsDrawerCollapsed] = useState(true)
-
-  const [activeMobileMenu, setActiveMobileMenu] = useState(false)
-
-  const handleMobileMenu = () => {
-    if (activeMobileMenu) {
-      // when desactive mobile menu then close it and scroll to top
-      window.scrollTo(0, 0)
-    }
-    setActiveMobileMenu(!activeMobileMenu)
-  }
-
   return (
     <Wrapper>
-      <Sidebar collapsed={isDrawerCollapsed} onCollapsed={value => setIsDrawerCollapsed(value)} />
-      <MainContent collapsed={isDrawerCollapsed}>
-        <URLWarning />
-        <Header activeMobileMenu={activeMobileMenu} handleMobileMenu={handleMobileMenu} />
-        <AppContent>
-          <Outlet />
-        </AppContent>
-        {activeMobileMenu && <MobileMenu activeMobileMenu={activeMobileMenu} handleMobileMenu={handleMobileMenu} />}
-        <Footer />
-      </MainContent>
+      <Outlet />
     </Wrapper>
   )
 }
