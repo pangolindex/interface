a750c0b0247f3686cb4e5a0e5be2f72126a3913e
Pedro Antonio <37405304+prodesert22@users.noreply.github.com>
2023-06-20 02:44:36 -0400

    feat: moved airdrop to components (#765)
    * feat: moved airdrop to components
    
    * fix: text font size
    
    * chore: component update
    
    ---------
    
    Co-authored-by: Sarju Hansaliya <sarju.hansaliya@gmail.com>

--- src/pages/Beta/Airdrop2/index.tsx
+++ src/pages/Beta/Airdrop2/index.tsx
@@ -1,54 +1,42 @@
 import React from 'react'
-import { PageWrapper, MainTitle, CenterText, Frame } from './styleds'
-import { Text, Box, useActiveWeb3React } from '@pangolindex/components'
-import { useChainId } from 'src/hooks'
+import { PageWrapper, CenterText, Frame } from './styleds'
+import { Text, Box, Airdrop, ComingSoon } from '@pangolindex/components'
 import { QuestionAnswer } from './QuestionBox'
-import { Chain, CHAINS } from '@pangolindex/sdk'
-import { activeAirdrops, commingSoonAirdrops, AirdropData, specialAirdrops } from 'src/constants/airdrop'
-import NotConnected from './NotConnected'
-import ChangeChain from './ChangeChain'
-import ClaimReward from './ClaimReward'
-import CommingSoon from './CommingSoon'
+import { activeAirdrops, comingSoonAirdrops, specialAirdrops } from 'src/constants/airdrop'
 
 const AirdropUI: React.FC = () => {
-  const { account } = useActiveWeb3React()
-  const chainId = useChainId()
-
-  const renderAirdrop = (chain: Chain, key: number, airdropData: AirdropData) => {
-    if (!account) {
-      return <NotConnected key={key} chain={chain} title={airdropData?.title} />
-    }
-    if (chainId !== chain?.chain_id) {
-      return <ChangeChain key={key} chain={chain} />
-    }
-    return <ClaimReward key={key} chain={chain} {...airdropData} />
-  }
-
   return (
     <PageWrapper>
       <Box paddingBottom="20px">
         <CenterText>
-          <MainTitle>Pangolin Going Crosschain</MainTitle>
-          <Text fontSize={[18, 14]} fontWeight={500} lineHeight="27px" color="text10">
-            And we are not empty handed!
+          <Text fontSize={[24, 32]} fontWeight={500} lineHeight="64px">
+            Pangolin Going Crosschain
           </Text>
+          {(activeAirdrops.length > 0 || specialAirdrops.length > 0) && (
+            <Text fontSize={[18, 14]} fontWeight={500} color="text10">
+              And we are not empty handed!
+            </Text>
+          )}
         </CenterText>
       </Box>
       <Frame>
-        {Object.entries(specialAirdrops).map(([chainId, airdrops], index) => {
-          return airdrops
-            ?.filter(data => data.active)
-            ?.map(data => renderAirdrop((CHAINS as any)[chainId], index, data))
-        })}
-        {Object.entries(activeAirdrops).map(([chainId, airdrop], index) => {
-          if (!!airdrop && airdrop.active) {
-            return renderAirdrop((CHAINS as any)[chainId], index, airdrop)
-          }
-          return null
-        })}
-        {commingSoonAirdrops.map((chain, index) => (
-          <CommingSoon key={index} chain={chain} />
-        ))}
+        {activeAirdrops.length === 0 && specialAirdrops.length === 0 && comingSoonAirdrops.length === 0 ? (
+          <Text fontWeight={500} fontSize={24} textAlign="center">
+            No airdrops active now.
+          </Text>
+        ) : (
+          <>
+            {activeAirdrops.map((airdrop, index) => (
+              <Airdrop {...airdrop} key={`${index}-${airdrop.contractAddress}`} />
+            ))}
+            {specialAirdrops.map((airdrop, index) => (
+              <Airdrop {...airdrop} key={`${index}-${airdrop.contractAddress}`} />
+            ))}
+            {comingSoonAirdrops.map((airdrop, index) => (
+              <ComingSoon {...airdrop} key={`${index}-${airdrop.token.address}`} />
+            ))}
+          </>
+        )}
       </Frame>
       <Box display="flex" flexDirection="column" alignItems="center" mb="20px">
         <Text fontSize={[32, 24]} fontWeight={500} lineHeight="66px" color="text10">
