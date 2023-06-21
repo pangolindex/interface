3ebe6eb69c6d68619f317c1fe948acfe32d3b09f
Bilal Corbacioglu <bilalcorbacioglu@gmail.com>
2023-06-21 18:20:09 +0300

    feat: added hedera airdrop page

--- src/pages/Beta/Airdrop2/QuestionBox/index.tsx
+++ src/pages/Beta/Airdrop2/QuestionBox/index.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react'
 import { Text } from '@pangolindex/components'
-import { StyledLogo, QuestionBox, StyledLogoMinus, Separator } from '../styleds'
+import { StyledLogo, QuestionBox, StyledLogoMinus, Separator } from '../airdrop/styleds'
 import PlusLogo from 'src/assets/images/plus.png'
 import MinusLogo from 'src/assets/images/minus.png'
 import { SubCategories, useGetKnowledgeData, QuestionAnswerType } from 'src/state/bridge/hooks'
 
-export const QuestionAnswer = () => {
+interface QuestionAnswerProps {
+  extraQuestions?: SubCategories[]
+}
+
+export const QuestionAnswer = ({ extraQuestions = [] }: QuestionAnswerProps) => {
   const [open, setOpen] = useState<number>()
   const [text, setText] = useState<string>('')
   const { data: questions } = useGetKnowledgeData(QuestionAnswerType.Airdrop, QuestionAnswerType.Undefined)
+  const allQuestions = extraQuestions.concat(questions || [])
 
   function activeLogo(key: number) {
     if (open === key) return <StyledLogoMinus src={MinusLogo} size={'20px'} height={'4px'} />
@@ -34,8 +39,8 @@ export const QuestionAnswer = () => {
   }
   return (
     <QuestionBox>
-      {questions &&
-        questions.map((question: SubCategories) => (
+      {allQuestions &&
+        allQuestions.map((question: SubCategories) => (
           <div
             key={question.id}
             onClick={() => {
