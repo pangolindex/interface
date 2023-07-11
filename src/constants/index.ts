ed4cabc4062dd55c7610c4a3f6053c584ad3c691
Pedro Antonio <37405304+prodesert22@users.noreply.github.com>
2023-07-11 07:11:34 -0400

    feat: update walletconnect to v2 (#775)
    * feat: update walletconnect to v2
    
    * fix: issues
    
    * chore: update components
    
    * chore: update components and sdk version
    
    ---------
    
    Co-authored-by: Sarju Hansaliya <sarju.hansaliya@gmail.com>

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,11 @@
 import { ChainId, JSBI, CHAINS } from '@pangolindex/sdk'
-import { ZERO_ADDRESS } from '@pangolindex/components'
+import {
+  ZERO_ADDRESS,
+  SUPPORTED_WALLETS,
+  SUPPORTED_EVM_CHAINS_ID,
+  PangolinWallet,
+  PangolinWalletConnectWallet
+} from '@pangolindex/components'
 
 export const LEGACY_PAGE = 'https://legacy.pangolin.exchange'
 export const PANGOLIN_TOKENS_REPO_RAW_BASE_URL = `https://raw.githubusercontent.com/pangolindex/tokens`
@@ -140,3 +146,33 @@ export enum AIRDROP_MENU_LINK {
 }
 
 export type CHILD_MENU_TYPES = BUY_MENU_LINK | POOL_MENU_LINK | AIRDROP_MENU_LINK
+
+const walletConnectProjectId = import.meta.env.VITE_WALLETCONNECT_PROJECTID
+
+export const supportedWallets: { [key: string]: PangolinWallet } = {
+  ...SUPPORTED_WALLETS
+}
+
+if (walletConnectProjectId) {
+  const rpcs = SUPPORTED_EVM_CHAINS_ID.reduce((acc, chainId) => {
+    const rpc: string | undefined = CHAINS[chainId as ChainId]?.rpc_uri
+
+    if (rpc) {
+      acc[chainId] = rpc
+    }
+    return acc
+  }, {} as { [chainId in number]: string })
+
+  const walletConnectWallet = new PangolinWalletConnectWallet({
+    rpcMap: rpcs,
+    projectId: walletConnectProjectId,
+    metadata: {
+      name: 'Pangolin',
+      description: 'Pangolin Dapp',
+      url: window.location.host,
+      icons: ['https://raw.githubusercontent.com/pangolindex/interface/master/public/images/384x384_App_Icon.png']
+    }
+  })
+
+  supportedWallets['WALLLET_CONNECT'] = walletConnectWallet
+}
