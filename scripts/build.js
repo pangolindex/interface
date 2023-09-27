d90c04236c1df790bb8b3735297666d33c3075d4
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2023-09-27 22:06:28 +0530

    fix: try building twice in vercel due to its memory issue
    While doing build second time, vercel uses cached files from first try
    so we are going to do build twice.

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,9 @@
 /* eslint-disable */
 const { spawn } = require('child_process')
 
-console.log('setting NODE_OPTIONS to --max-old-space-size=8192')
+// console.log('setting NODE_OPTIONS to --max-old-space-size=8192')
 // Step 1: Set the environment variable
-process.env.NODE_OPTIONS = '--max-old-space-size=8192'
+// process.env.NODE_OPTIONS = '--max-old-space-size=8192'
 
 // Step 2: Run the "yarn build" command
 const yarnBuild = spawn('yarn', ['vite:build'], {
@@ -20,6 +20,12 @@ yarnBuild.on('error', error => {
 yarnBuild.on('close', code => {
   if (code !== 0) {
     console.error(`"yarn build" exited with code ${code}`)
+    console.log('spawning another build')
+    spawn('yarn', ['vite:build'], {
+      stdio: 'inherit', // This option will pipe the stdio of the child process to the parent, effectively displaying it in the terminal.
+      shell: true, // This option will run the command in a shell, allowing for environment variable expansion.
+      env: process.env // Pass the modified environment variables to the child process.
+    })
   }
 })
 /* eslint-enable */
